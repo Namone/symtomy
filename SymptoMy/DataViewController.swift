7b17c4792358dc0776137983b28c6d84e0d1f49b
Alex Nordhausen <alex@macbook-pro-2.local>
2018-03-07 19:16:30 -0700

    Initial Commit

---
+++ SymptoMy/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  SymptoMy
+//
+//  Created by Alex Nordhausen on 3/7/18.
+//  Copyright © 2018 Alex Nordhausen. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
